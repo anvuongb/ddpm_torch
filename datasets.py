47cf3197b7d21272139edb815e6a364c70eec7d4
An Vuong <anvuongbk13@gmail.com>
2024-06-04 17:47:16 -0700

    add celeba
    add noise in log domain

--- datasets.py
+++ datasets.py
@@ -7,26 +7,28 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-def show_images_dataloader(out: str, data: DataLoader, cols:int=4):
+
+def show_images_dataloader(out: str, data: DataLoader, cols: int = 4):
     imgs = next(iter(data))
     """ Plots some samples from the dataset """
-    fig = plt.figure(figsize=(15,15)) 
+    fig = plt.figure(figsize=(15, 15))
     for i, img in enumerate(imgs):
-        plt.subplot(int(len(imgs)/cols) + 1, cols, i + 1)
-        plt.imshow(np.array((img.permute(1,2,0)+1)/2*255, dtype=int))
+        plt.subplot(int(len(imgs) / cols) + 1, cols, i + 1)
+        plt.imshow(np.array((img.permute(1, 2, 0) + 1) / 2 * 255, dtype=int))
         plt.axis("off")
     plt.tight_layout()
     fig.savefig(out)
-    plt.close('all')
+    plt.close("all")
 
-def show_images_batch(out: str, data: torch.Tensor, cols:int=4):
-    """ Plots some samples from the dataset """
-    fig = plt.figure(figsize=(15,15)) 
+
+def show_images_batch(out: str, data: torch.Tensor, cols: int = 4):
+    """Plots some samples from the dataset"""
+    fig = plt.figure(figsize=(15, 15))
     for i in range(data.shape[0]):
-        plt.subplot(int(data.shape[0]/cols) + 1, cols, i + 1)
+        plt.subplot(int(data.shape[0] / cols) + 1, cols, i + 1)
         # img = np.array((data[i].permute(1,2,0)+1)/2*255, dtype=int)
         # img = np.clip(img, 0, 255)
-        img = np.array(data[i].permute(1,2,0), dtype=int)
+        img = np.array(data[i].permute(1, 2, 0), dtype=int)
         img = np.clip(img, 0, 1)
         plt.imshow(img)
         plt.axis("off")
@@ -34,24 +36,64 @@ def show_images_batch(out: str, data: torch.Tensor, cols:int=4):
     if out is not None:
         fig.savefig(out)
 
+
 def cifar_data_transform(img_size=32):
     transform = [
-        # transforms.Resize((img_size, img_size)),
-        # transforms.RandomHorizontalFlip(),
-        transforms.Lambda(lambda t: t/255), # scale to [0, 1]
-        transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1] 
+        transforms.Grayscale(),
+        transforms.Resize((img_size, img_size)),
+        # transforms.ToTensor(),
+        transforms.Lambda(lambda t: t / 255),  # scale to [0, 1]
+        transforms.Lambda(lambda t: t + 1),  # Scale between [1, 2] for log
+        # transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1]
     ]
     transform = transforms.Compose(transform)
     return transform
 
-def mnist_data_transform(img_size=32):
+
+def celeba_data_transform(img_size=224):
     transform = [
+        transforms.Grayscale(),
         transforms.Resize((img_size, img_size)),
-        transforms.Lambda(lambda t: t/255), # scale to [0, 1]
-        transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1] 
+        # transforms.ToTensor(),
+        transforms.Lambda(lambda t: t / 255),  # scale to [0, 1]
+        transforms.Lambda(lambda t: t + 1),  # Scale between [1, 2] for log
+        # transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1]
     ]
     transform = transforms.Compose(transform)
     return transform
+
+
+def mnist_data_transform(img_size=32):
+    transform = [
+        transforms.Resize((img_size, img_size)),
+        transforms.Lambda(lambda t: t / 255),  # scale to [0, 1]
+        transforms.Lambda(lambda t: (t * 2) - 1),  # Scale between [-1, 1]
+    ]
+    transform = transforms.Compose(transform)
+    return transform
+
+
+class CelebADataset(Dataset):
+    def __init__(self, img_dir, transform):
+        self.img_dir = img_dir
+        self.img_paths = []
+        self.transform = transform
+        for f in os.listdir(self.img_dir):
+            if f.endswith("jpg"):
+                self.img_paths.append(os.path.join(self.img_dir, f))
+
+        self.len = len(self.img_paths)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        assert idx >= 0
+        assert idx < self.len
+        image = read_image(self.img_paths[idx])
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, 0
 
 
 class CifarDataset(Dataset):
@@ -60,25 +102,27 @@ class CifarDataset(Dataset):
         self.classes = classes
         if self.classes == "all":
             # default to all CIFAR classes
-            self.classes = ["airplane",
-                            "automobile",
-                            "bird",
-                            "cat",
-                            "deer",
-                            "dog",
-                            "frog",
-                            "horse",
-                            "ship",
-                            "truck",
-                            ]
+            self.classes = [
+                "airplane",
+                "automobile",
+                "bird",
+                "cat",
+                "deer",
+                "dog",
+                "frog",
+                "horse",
+                "ship",
+                "truck",
+            ]
         self.img_paths = []
         self.transform = transform
         for c in self.classes:
-            for f in os.listdir(os.path.join(self.img_dir,c)):
+            for f in os.listdir(os.path.join(self.img_dir, c)):
                 if f.endswith("jpg"):
                     self.img_paths.append(os.path.join(self.img_dir, c, f))
-        
-        self.len = len(self.img_paths)  
+
+        self.len = len(self.img_paths)
+
     def __len__(self):
         return self.len
 
@@ -88,23 +132,25 @@ class CifarDataset(Dataset):
         image = read_image(self.img_paths[idx])
         if self.transform is not None:
             image = self.transform(image)
-        return image
-    
+        return image, 0
+
+
 class MnistDataset(Dataset):
     def __init__(self, img_dir, classes, transform):
         self.img_dir = img_dir
         self.classes = classes
         if self.classes == "all":
             # default to all CIFAR classes
-            self.classes = [str(c) for c in range(10)] # ten digits
+            self.classes = [str(c) for c in range(10)]  # ten digits
         self.img_paths = []
         self.transform = transform
         for c in self.classes:
-            for f in os.listdir(os.path.join(self.img_dir,c)):
+            for f in os.listdir(os.path.join(self.img_dir, c)):
                 if f.endswith("jpg"):
                     self.img_paths.append(os.path.join(self.img_dir, c, f))
-        
-        self.len = len(self.img_paths)  
+
+        self.len = len(self.img_paths)
+
     def __len__(self):
         return self.len
 
@@ -115,10 +161,15 @@ class MnistDataset(Dataset):
         if self.transform is not None:
             image = self.transform(image)
         return image
-    
+
+
 if __name__ == "__main__":
     data_transforms = cifar_data_transform(img_size=32)
-    dataset = CifarDataset(img_dir="/home/anvuong/Desktop/datasets/CIFAR-10-images/train", classes="all", transform=data_transforms)
+    dataset = CifarDataset(
+        img_dir="/home/anvuong/Desktop/datasets/CIFAR-10-images/train",
+        classes="all",
+        transform=data_transforms,
+    )
 
     loader = DataLoader(dataset, batch_size=32, shuffle=True)
     show_images_dataloader(loader)
---
+++ diffusion_noise_removal.py
@@ -0,0 +1,310 @@
+from unet import UNet
+from torchvision import transforms
+import os
+from unet_simple import UNetSimple
+import torch.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import writer
+from torchvision.datasets import MNIST
+from torchvision.utils import save_image
+import torch
+import numpy as np
+from datasets import MnistDataset, mnist_data_transform, show_images_batch, cifar_data_transform, CifarDataset, celeba_data_transform, CelebADataset
+from torch.optim.lr_scheduler import LinearLR
+import tqdm
+import time
+from model_helpers import save_model, load_model
+import wandb
+
+
+def get_noise_schedule(num_steps, start=0.0001, end=0.02):
+    return torch.linspace(start, end, num_steps)
+
+
+# def forward_diffusion(x_0, t, alphas_cum, device="cpu"):
+#     noise = torch.randn_like(x_0).to(device)
+#     scale_t = torch.gather(alphas_cum, 1, t)
+#     scale_t = scale_t[:, :, None, None]
+#     x_t = torch.sqrt(scale_t) * x_0 + (1 - scale_t) * noise
+#     return x_t.to(device), noise
+
+def forward_diffusion_gbm(x_0, t, sigmas, device="cpu"):
+    noise = torch.randn_like(x_0).to(device)
+
+    log_x0 = torch.log(x_0)
+    
+    scale_t = torch.gather(sigmas, 1, t)
+    scale_t = scale_t[:, :, None, None]
+    scale_0 = torch.gather(sigmas, 1, torch.zeros_like(t))
+    scale_0 = scale_0[:, :, None, None]
+
+    mean = log_x0 -0.5*(scale_t - scale_0)
+    var = torch.sqrt(scale_t-scale_0)*noise
+    log_xt = mean + var
+    return log_xt.to(device), noise
+
+
+# TODO: finish this sample function
+@torch.no_grad()
+def one_step_denoising(
+    model,
+    x,
+    t,
+    alphas_cum,
+    alphas_cum_prev,
+    alphas,
+    betas,
+    noise_sched="from_gaussian",
+    device="cpu",
+):
+
+    alpha_t = torch.gather(alphas, 1, t)
+    alpha_t = alpha_t[:, :, None, None]
+
+    beta_t = torch.gather(betas, 1, t)
+    beta_t = beta_t[:, :, None, None]
+
+    scale_t = torch.gather(alphas_cum, 1, t)
+    scale_t = scale_t[:, :, None, None]
+
+    scale_t_prev = torch.gather(alphas_cum_prev, 1, t)
+    scale_t_prev = scale_t_prev[:, :, None, None]
+
+    z = torch.randn_like(x)
+
+    if t.squeeze()[0] == 0:
+        z = z * 0
+
+    noise_pred = model(x, t.squeeze())
+    noise_pred = torch.clamp(noise_pred, -1, 1)
+
+    # noise = beta
+    noise_scale = torch.sqrt(beta_t)
+    # if noise_sched == "from_gaussian":
+    #     # noise = scaling
+    #     noise_scale = torch.sqrt((1 - scale_t_prev) / (1 - scale_t) * beta_t)
+
+    x_out = (1 / torch.sqrt(alpha_t)) * (
+        x - beta_t / torch.sqrt(1 - scale_t) * noise_pred
+    ) + noise_scale * z
+
+    # noise = beta scale
+
+    return x_out.to(device)
+
+
+@torch.no_grad()
+def sample_from_noise(
+    x_0: torch.Tensor,
+    model: UNet,
+    timesteps: int,
+    alphas_cum: torch.Tensor,
+    alphas_cum_prev: torch.Tensor,
+    alphas: torch.Tensor,
+    betas: torch.Tensor,
+    device: str = "cpu",
+):
+    x = torch.randn_like(x_0).to(device)
+    for t_ in tqdm.tqdm(reversed(range(timesteps)), total=timesteps):
+        t = torch.ones(size=(x.shape[0], 1), dtype=int) * t_
+        t = t.to(device)
+        x = one_step_denoising(
+            model, x, t, alphas_cum, alphas_cum_prev, alphas, betas, device=device
+        )
+    return x
+
+
+if __name__ == "__main__":
+    for i in range(torch.cuda.device_count()):
+        print(
+            "found GPU",
+            torch.cuda.get_device_name(i),
+            "capability",
+            torch.cuda.get_device_capability(i),
+        )
+
+    # # Init dataset Mnist
+    # batch_size = 256
+
+    # tf = transforms.Compose(
+    #     [transforms.Resize((32, 32)), transforms.ToTensor(),
+    #      transforms.Lambda(lambda t: t + 0.5)]  # to be able to take log
+    # )  # mnist is already normalised 0 to 1
+
+    # data = MNIST("./data", train=True, download=True, transform=tf)
+    # loader = DataLoader(
+    #     data, batch_size=batch_size, shuffle=True, num_workers=5, drop_last=True
+    # )
+
+    # # Init dataset Cifar10
+    # batch_size = 256
+    # data_transform = cifar_data_transform()
+    # data = CifarDataset(
+    #     img_dir="/home/anvuong/Desktop/datasets/CIFAR-10-images/train",
+    #     classes="all",
+    #     transform=data_transform,
+    # )
+    # loader = DataLoader(data, batch_size=batch_size, shuffle=True, num_workers=5, drop_last=True)
+
+    # # Init dataset celeba
+    batch_size = 16
+    data_transform = celeba_data_transform(128)
+    data = CelebADataset(
+        img_dir="/home/anvuong/Desktop/datasets/CelebA/Img/img_celeba",
+        transform=data_transform,
+    )
+    loader = DataLoader(data, batch_size=batch_size, shuffle=True, num_workers=5, drop_last=True)
+
+
+    # Init model
+    device = "cuda:1"
+    unet_conf = {
+        "init_channels": 64,
+        "in_channels": 1,
+        "out_channels": 1,
+        "num_res_blocks": 2,
+        "attn_resolutions": (32, 16,),
+        "input_img_resolution": 128,
+        "channels_multipliers": (2, 2, 2),
+    }
+    model = UNet(**unet_conf).to(device)
+
+    total_params = sum([p.numel() for p in model.parameters()])
+    print("Model initialized, total params = ", total_params)
+
+    # Init diffusion params
+    T = 500
+    sigmas = get_noise_schedule(T)
+    betas = get_noise_schedule(T)
+    alphas = 1 - betas
+    alphas_cum = torch.cumprod(alphas, dim=0)
+    alphas_cum_prev = torch.cat([torch.Tensor([1.0]), alphas_cum[:-1]])
+
+    # repeat into batch_size for easier indexing by t
+    sigmas = sigmas.unsqueeze(0).repeat(batch_size, 1).to(device)
+    betas = betas.unsqueeze(0).repeat(batch_size, 1).to(device)
+    alphas = alphas.unsqueeze(0).repeat(batch_size, 1).to(device)
+    alphas_cum = alphas_cum.unsqueeze(0).repeat(batch_size, 1).to(device)
+    alphas_cum_prev = alphas_cum_prev.unsqueeze(0).repeat(batch_size, 1).to(device)
+
+    # torch.autograd.set_detect_anomaly(True)
+
+    # # Get a sample batch
+    x = next(iter(loader))
+    x = x[0]
+    # print(x.shape)
+    t = torch.randint(low=1, high=T, size=(batch_size, 1))
+    print(x.shape)
+    # Exp name
+    exp_name = "cifar-noise-removal"
+
+    # init tensorboard writer
+    current_time = time.time()
+    # tb_writer = writer.SummaryWriter(f"logdir/{exp_name}_{current_time}")
+    tb_writer = writer.SummaryWriter(f"logdir/{exp_name}")
+    tb_writer.add_graph(
+        model=model, input_to_model=[x.to(device), t.squeeze().to(device)]
+    )
+
+    # train params
+    epochs = 100
+    start_epoch = 0
+    # load_model = True
+
+    # load from save
+    # if load_model:
+    #     model.load_state_dict(
+    #         torch.load(
+    #             "/home/anvuong/Desktop/codes/ddpm_torch/models/Mnist-all-3/model.pkl",
+    #             map_location=device,
+    #         )
+    #     )
+    # start_epoch = 0
+
+    # re-init dataloader
+    # loader = DataLoader(data, batch_size=batch_size, drop_last=True)
+    loader = DataLoader(
+        data, batch_size=batch_size, shuffle=True, num_workers=5, drop_last=True
+    )
+
+    # Init optimizer
+    lr = 1e-5
+    opt = torch.optim.Adam(model.parameters(), lr=lr)
+    # lr_schedule = LinearLR(opt, total_iters=epochs)
+    opt.zero_grad()
+
+    # TODO: currently loss keeps increasing with epochs, shit is happening
+    # TODO: need to fix this
+    total = len(loader)
+    print("num batches = ", total)
+    wandb.init(
+        project="noise-removal",
+        config={
+            "load_model": load_model,
+            "learning_rate": lr,
+            "epochs": epochs,
+            "start_epoch": start_epoch,
+            "unet_config": unet_conf,
+        },
+    )
+    for e in np.arange(start_epoch, start_epoch + epochs):
+        loss_ema = None
+        pbar = tqdm.tqdm(enumerate(loader), total=total)
+        for idx, x in pbar:
+            x = x[0]
+            x = x.to(device)
+            t = torch.randint(low=0, high=T, size=(batch_size, 1)).to(device)
+
+            log_x_in, noise_in = forward_diffusion_gbm(x, t, sigmas, device=device)
+            # x_in, noise_in = forward_diffusion(x, t, alphas_cum, device=device)
+            # x_out = forward_diffusion(x, t, alphas_cum, device=device)
+
+            noise_pred = model(log_x_in, t.squeeze())
+            loss = torch.nn.functional.mse_loss(noise_in, noise_pred, reduction="mean")
+            loss.backward()
+            tb_writer.add_scalar("loss", loss.item(), e * total + idx)
+
+            if loss.item() == torch.nan:
+                raise Exception("loss becomes nan")
+
+            if loss_ema is None:
+                loss_ema = loss.item()
+            else:
+                loss_ema = 0.95 * loss_ema + 0.05 * loss.item()
+            pbar.set_description(f"loss: {loss_ema:.4f}")
+
+            opt.step()
+            
+            # log to wandb
+            wandb.log({"loss": loss.item()})
+
+        # linear lrate decay
+        opt.param_groups[0]["lr"] = lr * (1 - e / epochs)
+        # lr_schedule.step()
+
+        print(f"epoch {e} loss={loss.item()}")
+        # show image and save model every 100 epochs
+        # if e % 1 == 0:
+        #     print("Generating sample images")
+        #     x_in = torch.ones(16, *x.shape[1:])
+        #     x_denoised = sample_from_noise(
+        #         x_in,
+        #         model,
+        #         T,
+        #         alphas_cum,
+        #         alphas_cum_prev,
+        #         alphas,
+        #         betas,
+        #         device=device,
+        #     )
+        #     x_denoised = x_denoised.to("cpu")
+        #     if not os.path.exists(f"./sampling_images/{exp_name}"):
+        #         os.makedirs(f"./sampling_images/{exp_name}")
+        #     # show_images_batch(
+        #     #     f"sampling_images/{exp_name}/sample_epoch_{e}.png", x_denoised
+        #     # )
+        #     # show_images_batch(f"sampling_images/{exp_name}/latest.png", x_denoised)
+        #     save_image(x_denoised, f"sampling_images/{exp_name}/sample_epoch_{e}.png")
+        #     save_image(x_denoised, f"sampling_images/{exp_name}/latest.png")
+        #     wandb.save(f"sampling_images/{exp_name}/*.png")
+        #     save_model(f"models/{exp_name}/model.pkl", model)
